ad6166343627ed0f20549ae73ccf589510522cd2
Tom Softreck <info@softreck.dev>
2025-07-16 19:11:55 +0200

    update

---
+++ video-to-svg/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="video2svg",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "opencv-python",
+        "numpy",
+        "svgwrite",
+        "PyYAML"
+    ],
+    entry_points={
+        "console_scripts": [
+            "video2svg=cli:main",
+        ],
+    },
+    python_requires=">=3.6",
+    description="Convert videos to SVG animations",
+    author="",
+    author_email="",
+    url="",
+)
